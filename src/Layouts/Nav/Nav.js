5e951d274ab566452fca624a27633cd3a289de69
mends albert <mendsalbert@gmail.com>
2021-06-11 22:03:07 +0100

    mobile version comleted

--- src/Layouts/Nav/Nav.js
+++ src/Layouts/Nav/Nav.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HandBurger from "../../images/icon-hamburger.svg";
 import Logo from "../../images/logo.svg";
 export default function Nav() {
+  // useEffect(() => {
+  window.onscroll = function () {
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    if (window.pageYOffset > 140) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  };
+  // console.log(window.pageYOffset);
+  // console.log(sticky);
+  // }, []);
   return (
     <header>
-      <nav className="nav">
+      <nav className="nav" id="navbar">
         <img src={Logo} className="nav__logo" />
         {/* <div className="nav__handburger"> */}
         <input type="checkbox" className="nav__checked" id="toggle" />
