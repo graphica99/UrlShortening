dcaed3c4a5958636ea98b302c6739c2dee64817e
mends albert <mendsalbert@gmail.com>
2021-06-11 19:20:57 +0100

    mobile version comleted

--- src/Layouts/Nav/Nav.js
+++ src/Layouts/Nav/Nav.js
@@ -1,17 +1,26 @@
 import React from "react";
-// import HandBurger from "../../images/icon-hamburger.svg";
+import HandBurger from "../../images/icon-hamburger.svg";
 import Logo from "../../images/logo.svg";
 export default function Nav() {
   return (
     <header>
       <nav className="nav">
         <img src={Logo} className="nav__logo" />
-        {/* <div className='nav__right'> */}
+        {/* <div className="nav__handburger"> */}
+        <input type="checkbox" className="nav__checked" id="toggle" />
+        <label for="toggle">
+          <img src={HandBurger} className="nav__handburger-icon" />
+        </label>
+        {/* </div> */}
         <div className="nav__left">
           <ul className="nav__items">
             <li className="nav__item">Features</li>
             <li className="nav__item">Pricing</li>
             <li className="nav__item">Resources</li>
+
+            <li className="nav__item-mb">Login</li>
+            <br />
+            <li className="nav__item-mb btn-m u-mb-medium">Sign Up</li>
           </ul>
         </div>
         <div>
@@ -22,13 +31,6 @@ export default function Nav() {
             Sign Up
           </a>
         </div>
-
-        {/* <div className="nav__handburger">
-          <input type="checkbox" className="nav__checked" id="toggle" />
-          <label for="toggle">
-            <img src={HandBurger} className="nav_handburger-icon" />
-          </label>
-        </div> */}
       </nav>
     </header>
   );
